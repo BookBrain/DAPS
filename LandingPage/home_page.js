083dd2b64f03903ae7d1e108bd78f708398d0acc
Xalmon <xalmonoyefule@gmail.com>
2023-12-05 16:30:39 +0100

    [12]

--- LandingPage/home_page.js
+++ LandingPage/home_page.js
@@ -2,10 +2,35 @@ document.addEventListener('DOMContentLoaded', function () {
     const addBookForm = document.getElementById('add-book-form');
     addBookForm.addEventListener('submit', handleAddBookForm);
 
-
     const showBookListButton = document.getElementById('show-book-list-button');
     const bookListPopup = document.getElementById('book-list-popup');
     const closePopupButton = document.getElementById('close-popup-button');
+
+    const searchForm = document.getElementById('search-form');
+    const searchInput = document.getElementById('search-input');
+
+    searchForm.addEventListener('submit', function (event) {
+        event.preventDefault();
+        const searchTerm = searchInput.value.trim().toLowerCase();
+        searchBooks(searchTerm);
+    });
+
+    function searchBooks(query) {
+        const bookListContainer = document.getElementById('book-list');
+        const bookItems = bookListContainer.getElementsByClassName('book-item');
+
+        for (const bookItem of bookItems) {
+            const title = bookItem.querySelector('p:nth-child(1)').textContent.toLowerCase();
+            const author = bookItem.querySelector('p:nth-child(2)').textContent.toLowerCase();
+            const genre = bookItem.querySelector('p:nth-child(3)').textContent.toLowerCase();
+
+            if (title.includes(query) || author.includes(query) || genre.includes(query)) {
+                bookItem.style.display = 'block';
+            } else {
+                bookItem.style.display = 'none';
+            }
+        }
+    }
 
     function handleAddBookForm(event) {
         event.preventDefault();
@@ -55,16 +80,19 @@ document.addEventListener('DOMContentLoaded', function () {
 });
 
 function displayBookList() {
-    const bookListContainer = document.getElementById('book-list-ul');
+    const bookListContainer = document.getElementById('book-list-container');
     bookListContainer.innerHTML = '';
 
-    window.bookInventory.forEach((book, index) => {
-        const listItem = document.createElement('li');
-        listItem.textContent = `${book.title} by ${book.author}`;
-        listItem.classList.add('book-list-item');
+    window.bookInventory.forEach(book => {
+        const bookItem = document.createElement('div');
+        bookItem.classList.add('book-item');
+        bookItem.innerHTML = `
+            <p>Title: ${book.title}</p>
+            <p>Author: ${book.author}</p>
+            <p>Genre: ${book.genre}</p>
+            <button class="delete-button" onclick="deleteBook(this)">Delete</button>
+        `;
 
-        listItem.addEventListener('click', () => showBookDetails(index));
-
-        bookListContainer.appendChild(listItem);
+        bookListContainer.appendChild(bookItem);
     });
 }
